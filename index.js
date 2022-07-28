3c057498339a657332deea86b548e5f4a48d721a
Dmitry Baranov <zyzychka1@gmail.com>
2022-07-28 18:52:19 +0300

    deleting Procfile

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const { mysql2 } = require('mysql2');
 require('dotenv').config();
 
 const States = {};
-const connection = mysql2.createConnection({
-    host: process.env,
-    user: process.env.DB_USER
-})
+// const connection = mysql2.createConnection({
+//     host: process.env,
+//     user: process.env.DB_USER
+// })
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start((ctx) => ctx.replyWithMarkdownV2("[Вопрос 1](https://i.pinimg.com/736x/7b/19/bc/7b19bc6569545fbabdb941a5eed1f260.jpg)", {
