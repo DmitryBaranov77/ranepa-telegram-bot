87a8733428d0bf119d3f141cb1a21456605ce0ea
Dmitry Baranov <zyzychka1@gmail.com>
2022-07-28 18:41:15 +0300

    adding Procfile

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const {
-    Telegraf
-} = require('telegraf');
+const { Telegraf } = require('telegraf');
+const { mysql2 } = require('mysql2');
 require('dotenv').config();
+
 const States = {};
+const connection = mysql2.createConnection({
+    host: process.env,
+    user: process.env.DB_USER
+})
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-// bot.start((ctx) => ctx.replyWithSticker("CAACAgIAAxkBAAIBqmLhRjmO2hrQVJ95SR-J3NON85a4AAKuAAM9zOMzAAE7Fuq_Cg_WKQQ"));
 bot.start((ctx) => ctx.replyWithMarkdownV2("[Вопрос 1](https://i.pinimg.com/736x/7b/19/bc/7b19bc6569545fbabdb941a5eed1f260.jpg)", {
     disable_web_page_preview: true
 }));
